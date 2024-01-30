76be3041f9d5eae22598a76a56a96f2f9d56ebec
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2024-01-30 12:09:36 -0600

    actividaddeprogramacion1.cpp

---
+++ actividaddeprogramacion1.cpp
@@ -0,0 +1,20 @@
+/**********Presentación***********************
+NOMBRE: Eduardo Domínguez Padilla
+FECHA: 29 de enero de 2024
+PROGRAMA: actividaddeprogramacion1.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERA EN COMPUTACIÓN / 2DO SEMESTTRE
+PROFESOR Sergio Franco Casillas
+DESCRIPCIÓN: Programa que solamente imprime mis datos personales que previamente ingrese dentro del código
+**************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nombre: Eduardo Dominguez Padilla\nEdad: 18 años\nLugar de residencia: Teocaltitan de Guadalupe\nFecha: 29/01/2024\nCarrera: Ingenieria en computacion\nSemestre: 2do" << endl;
+
+    return 0;
+}
