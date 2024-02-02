8ade233ef51929e75eed7147a02d01c3812a2b75
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2024-02-02 11:16:00 -0600

    Prueba para subir ejecutables

--- prueba.cpp
+++ prueba.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 using namespace std;
 main(){
-
+  string nombre;
+  nombre = "Eduardo";
   cout<<"Con esta modificación vamos a dar por terminada la documentación de la clase de hoy:D"<<endl;
-
+  cout << nombre;
   return 0;
 }
